c4f8fd0060af40c06376b6bb75698f14399a92c7
iAlhasan <Prowess@live.com>
2019-02-26 21:20:48 -0500

    InClass5

---
+++ Create dbo.Departments.sql
@@ -0,0 +1,16 @@
+﻿USE [Organization]
+GO
+
+/****** Object: Table [dbo].[Departments] Script Date: 2/26/2019 9:12:36 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Departments] (
+    [Code] VARCHAR (50) NOT NULL,
+    [Name] VARCHAR (50) NULL
+);
+
+
---
+++ Create dbo.Employees.sql
@@ -0,0 +1,18 @@
+﻿USE [Organization]
+GO
+
+/****** Object: Table [dbo].[Employees] Script Date: 2/26/2019 9:16:20 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Employees] (
+    [Id]        INT          NOT NULL,
+    [FirstName] VARCHAR (50) NULL,
+    [LastName]  VARCHAR (50) NULL,
+    [Code]      VARCHAR (50) NOT NULL
+);
+
+
